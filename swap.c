e81084a41c340a6125d2dba7b8266556183b0762
aniketpal08 <aniket.pal@somaiya.edu>
2025-07-30 14:56:00 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -8,16 +8,16 @@ int main() {
 
   // swapping
 
-  // a = (initial_a - initial_b)
+
   a = a - b;   
 
-  // b = (initial_a - initial_b) + initial_b = initial_a
+
   b = a + b;
 
-  // a = initial_a - (initial_a - initial_b) = initial_b
+
   a = b - a;
 
-  // %.2lf displays numbers up to 2 decimal places
+  
   printf("After swapping, a = %.2lf\n", a);
   printf("After swapping, b = %.2lf", b);
 
