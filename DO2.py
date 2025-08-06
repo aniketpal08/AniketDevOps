68eb8769a4f9daa3c8a627c55154a642ffcd8dad
aniketpal08 <aniket.pal@somaiya.edu>
2025-08-06 13:40:10 +0530

    Update and rename DO2.txt to DO2.py

---
+++ DO2.py
@@ -0,0 +1,13 @@
+#This for my dev ops 2nd experiment
+
+num1 = 5
+num2 = 10
+print("Before swap:")
+print("num1 =", num1)
+print("num2 =", num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("\nAfter swap:")
+print("num1 =", num1)
+print("num2 =", num2)
